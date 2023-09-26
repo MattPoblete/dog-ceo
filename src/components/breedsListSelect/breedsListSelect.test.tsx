c0a5b574c24d2aefeb38c2527d65669b643e66de
Matias <matias.pobleteduran@gmail.com>
2023-09-26 06:52:28 -0300

    feature/breedsListSelect-component-created

---
+++ src/components/breedsListSelect/breedsListSelect.test.tsx
@@ -0,0 +1,49 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { describe, it, expect } from 'vitest'
+import  configureStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+import BreedsListSelect from './breedsListSelect.tsx'
+
+const mockStore = configureStore([])
+describe('<BreedsListSelect/>', () => {
+    const initialState = {
+        breeds: {
+            isLoading: false,
+            breedsList: {
+                "australian": [
+                    "shepherd"
+                ],
+                "basenji": [],
+                "bulldog": [
+                    "boston",
+                    "english",
+                    "french"
+                  ],
+            },
+            selectedBreed: {
+                breed: "australian",
+                subBreed: ['shepherd']
+            },
+            error: null
+        }
+    }
+    const store = mockStore(initialState)
+    render(
+        <Provider store={store}>
+            <BreedsListSelect />
+        </Provider>
+    )
+
+    it('should render the title', ()=>{
+        screen.getByText(/Breeds:/i)
+    })
+
+    it('should create a selector with the provided breeds', ()=>{
+        screen.getByText('bulldog')
+        screen.getByText('australian')
+    })
+   
+    it('should render a new selector if the selected breed has one or more sub-breeds', ()=>{
+        screen.getByText('shepherd')
+    })
+})
---
+++ src/components/breedsListSelect/breedsListSelect.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import { selectBreed, selectSubBreed } from '../../redux/reducers/breedsReducer'
+const BreedsListSelect = () => {
+  const breeds = useSelector((state: any)=>state.breeds)
+  const selectedBreed = breeds.selectedBreed
+  const subBreeds = breeds.selectedBreed.subBreed
+  const dispatch = useDispatch()
+  const handleChange = (event: any)=> {
+    const targetBreed = event.target.value
+    const newSelectedBreed = {
+      breed: targetBreed,
+      subBreed: breeds.breedsList[targetBreed]
+    }
+    dispatch(selectBreed(newSelectedBreed))
+  }
+  const handleSubBreedChange = (event: any) => {
+    const subBreed = event.target.value
+    dispatch(selectSubBreed(subBreed))
+  }
+
+  useEffect(()=>{
+    
+  },[selectedBreed])
+  return (
+    <div>
+      <h1>Breeds:</h1>
+      <select value={selectedBreed.breed} onChange={handleChange}>
+        {Object.keys(breeds.breedsList).map((breed, i) => (
+          <option key={i}>{breed}</option>
+        ))}
+      </select>
+      {subBreeds.length > 0 &&
+      <select value={selectedBreed.selectedSubBreed} onChange={handleSubBreedChange}>
+        {selectedBreed.subBreed.map((subBreed: string, key: number)=> (
+          <option key={key}>{subBreed}</option>
+        ))}
+      </select>
+      }
+    </div>
+  )
+}
+
+export default BreedsListSelect
